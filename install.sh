7368cfe71196ee19adc8156a0eb2d7a7851dc081
biyivi <75817113+biyivi@users.noreply.github.com>
2021-07-07 16:44:45 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+echo "Iniciando  instalacion..."
+sleep 2 
+pkg install python -y 
+clear
+pip install colorama 
+clear
+pip install tqdm 
+echo "La instalacion se ha completado.."
+var1="1"
+var2="2"
+echo "Iniciar script"
+echo "1) Si"
+echo "2) Salir"
+read -p ">> " resp
+if "$resp" == "var1"
+then
+python biyivi_Ataque_DDoS.py
+else
+echo "Para iniciar el scripit debe escribir: python biyivi_Ataque_DDoS.py"
+echo ":D"
